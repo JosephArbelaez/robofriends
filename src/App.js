bee10de960df33895642d3b6bc076b14e0049f6c
Joseph Arbelaez <josepharbelaez@gmail.com>
2018-10-22 15:53:31 -0500

    Collecting data from JSON

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import SearchBox from './SearchBox';
 import './App.css';
 /*
     This = The App
+    States are "Smart Components" and holds the state of the app.
+    constructor is blank on page load
+    The page is then rendered with loading
+    Then componentDidMount runs to get the robos
+    Then the page is rendered again with the robos, the loading text is removed.
 */
 
 class App extends React.Component {
@@ -16,20 +21,39 @@ class App extends React.Component {
         }
     }
 
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: robots }));
+        
+    }
+
+    // Function that changes the state based on what the value of the search text
+    
     onSearchChange= (event) => {
         this.setState({ searchfield: event.target.value})
     }
+
     render(){
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        )
+
+        /*
+            This if statement is just in case 
+            there are lots of information and it is slow to respond
+        */
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else{
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            )
+        }
     }
 }
 

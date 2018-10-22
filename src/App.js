0d04bf6f2d9a14c2bf2c88caa5064a3e4ecc31f9
Joseph Arbelaez <josepharbelaez@gmail.com>
2018-10-22 15:15:09 -0500

    Styliize

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CardList from './CardList'
 import { robots } from './robots';
 import SearchBox from './SearchBox';
-
+import './App.css';
 /*
     This = The App
 */
@@ -25,7 +25,7 @@ class App extends React.Component {
         })
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots}/>
             </div>

8606373ce331ea65693bb58e599d0d17d6810af9
Joseph Arbelaez <josepharbelaez@gmail.com>
2018-11-05 10:54:19 -0600

    Redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { Provider, connect } from 'react-redux';
+import {searchRobots} from './reducers';
 
-ReactDOM.render(<App /> , document.getElementById('root'));
+const store = createStore(searchRobots)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(<Provider>
+                    <App store = { store }/>
+                </Provider>, document.getElementById('root'));
 serviceWorker.unregister();
